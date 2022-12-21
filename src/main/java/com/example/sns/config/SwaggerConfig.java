b73275d182a6054acf1c1cab29a7c9ad8c83804a
Jin-hyuck <gjwlsgur4866@gmail.com>
2022-12-21 21:26:24 +0900

    [Test] ci/cd 테스트

--- src/main/java/com/example/sns/config/SwaggerConfig.java
+++ src/main/java/com/example/sns/config/SwaggerConfig.java
@@ -15,6 +15,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
--- src/main/java/com/example/sns/controller/UserApiController.java
+++ src/main/java/com/example/sns/controller/UserApiController.java
@@ -5,6 +5,7 @@ import com.example.sns.dto.UserJoinResponseDto;
 import com.example.sns.dto.UserLoginRequestDto;
 import com.example.sns.dto.UserLoginResponseDto;
 import com.example.sns.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +19,14 @@ public class UserApiController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "회원가입", notes="성공시 결과{userId, userName}, 유저 이름 중복 - 409에러")
     @PostMapping("/join")
     public RsData join(@RequestBody UserJoinRequestDto requestDto) {
         UserJoinResponseDto responseDto = userService.join(requestDto);
         return RsData.success(responseDto);
     }
 
+    @ApiOperation(value = "로그인", notes="성공시 결과{token} 유저 아이디 못찾음 - 404에러, 유저 패스워드 다름 - 401에러")
     @PostMapping("login")
     public RsData login(@RequestBody UserLoginRequestDto requestDto) {
         UserLoginResponseDto responseDto = userService.login(requestDto);
