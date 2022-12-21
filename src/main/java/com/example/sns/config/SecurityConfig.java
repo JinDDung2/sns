fa55c6e0cf33c2a2bc92772642790b422bcd7505
Jin-hyuck <gjwlsgur4866@gmail.com>
2022-12-21 16:19:58 +0900

    [Feat] 스프링 시큐리티 수정

--- src/main/java/com/example/sns/config/SecurityConfig.java
+++ src/main/java/com/example/sns/config/SecurityConfig.java
@@ -1,17 +1,14 @@
 package com.example.sns.config;
 
-import com.example.sns.jwt.JwtTokenFilter;
 import com.example.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -29,13 +26,12 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeHttpRequests()
-                .antMatchers("/api/v1/users/join", "api/v1/users/login").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
+                .antMatchers("/api/v1/**").permitAll()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
