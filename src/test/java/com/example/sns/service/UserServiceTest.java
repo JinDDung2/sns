5d16da83ece5d8d119297eef5f62fe88bc4ada3a
Jin-hyuck <gjwlsgur4866@gmail.com>
2022-12-27 11:03:29 +0900

    [Test] 유저 서비스 테스트 전체 수정 1. SpringBootTest -> MockTest 2. 로그인 성공, 실패 테스트 추가 3. 역할 수정 테스트 추가

--- src/test/java/com/example/sns/service/UserServiceTest.java
+++ src/test/java/com/example/sns/service/UserServiceTest.java
@@ -1,55 +1,122 @@
 package com.example.sns.service;
 
 import com.example.sns.dto.UserJoinRequestDto;
+import com.example.sns.dto.UserJoinResponseDto;
+import com.example.sns.dto.UserLoginRequestDto;
+import com.example.sns.dto.UserLoginResponseDto;
 import com.example.sns.entity.User;
 import com.example.sns.exception.SpringBootAppException;
+import com.example.sns.jwt.JwtTokenUtils;
 import com.example.sns.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Optional;
+
+import static com.example.sns.entity.Role.ADMIN;
+import static com.example.sns.entity.Role.USER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@Transactional
 class UserServiceTest {
 
-    @Autowired
+    UserRepository userRepository = Mockito.mock(UserRepository.class);
+    BCryptPasswordEncoder passwordEncoder = Mockito.mock(BCryptPasswordEncoder.class);
+    JwtTokenUtils jwtTokenUtils = Mockito.mock(JwtTokenUtils.class);
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+    private long expiredTimeMs = 1000 * 60 * 60;
     UserService userService;
 
-    @Autowired
-    UserRepository userRepository;
+    @BeforeEach
+    void setup() {
+        userService = new UserService(userRepository, passwordEncoder, jwtTokenUtils);
+    }
+
+    User user = User.builder()
+            .id(1)
+            .userName("user")
+            .password("user")
+            .role(USER)
+            .build();
+    User admin = User.builder()
+            .id(0)
+            .userName("admin")
+            .password("admin")
+            .role(ADMIN)
+            .build();
+
+    UserLoginRequestDto loginRequest = new UserLoginRequestDto(user.getUserName(), user.getPassword());
 
     @Test
     void 회원가입_성공(){
-        // given
-        UserJoinRequestDto requestDto = new UserJoinRequestDto("testName", "testPwd");
-        // when
-        userService.join(requestDto);
-        User savedUser = userRepository.findByUserName("testName").get();
-        // then
-        assertEquals(requestDto.getUserName(), savedUser.getUserName());
+        given(userRepository.existsByUserName(user.getUserName())).willReturn(false);
+        given(passwordEncoder.encode(user.getPassword())).willReturn(user.getPassword());
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+
+        UserJoinRequestDto requestDto = new UserJoinRequestDto(user.getUserName(), user.getPassword());
+        UserJoinResponseDto response = userService.join(requestDto);
+
+        assertEquals(response.getUserName(), user.getUserName());
     }
 
     @Test
     void 회원가입_실패_중복아이디(){
-        // given
-        User existedUser = User.builder()
-                .userName("testName")
-                .password("testPwd")
-                .build();
+        UserJoinRequestDto requestDto = new UserJoinRequestDto("user", "user");
 
-        userRepository.save(existedUser);
+        when(userRepository.existsByUserName(requestDto.getUserName())).thenReturn(true);
 
-        UserJoinRequestDto requestDto = new UserJoinRequestDto("testName", "testPwd");
-        // when
-        // then
         assertThrows(SpringBootAppException.class, () -> {
             userService.join(requestDto);
         });
 
+    }
+
+    @Test
+    void 로그인_성공() {
+        given(jwtTokenUtils.createToken(user.getUserName(), secretKey, expiredTimeMs)).willReturn("jwt");
+        given(userRepository.findByUserName(user.getUserName())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(any(String.class), any(String.class))).willReturn(true);
+
+        UserLoginResponseDto responseDto = userService.login(loginRequest);
+
+        assertEquals(responseDto.getJwt(), "jwt");
+    }
+
+    @Test
+    void 로그인_실패_아이디_못찾음() {
+        when(userRepository.findByUserName(user.getUserName())).thenReturn(Optional.empty());
+        assertThrows(SpringBootAppException.class, () -> {
+            userService.login(loginRequest);
+        });
+    }
+
+    @Test
+    void 로그인_실패_비밀번호_틀림() {
+        when(userRepository.findByUserName(user.getUserName())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(any(String.class), any(String.class))).thenReturn(false);
+
+        assertThrows(SpringBootAppException.class, () -> {
+            userService.login(loginRequest);
+        });
+
+    }
+
+    @Test
+    void 역할수정() {
+        given(userRepository.findByUserName(admin.getUserName())).willReturn(Optional.of(admin));
+        given(userRepository.findByUserName(user.getUserName())).willReturn(Optional.of(admin));
+
+        User changeUser = admin.upgradeAdmin(user);
+
+        assertEquals(changeUser.getRole(), ADMIN);
     }
 
 }
