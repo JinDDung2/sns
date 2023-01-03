3bbbcdf41f128175434c8aa1a7106d6293abca1c
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-01-03 11:42:04 +0900

    [Feat] 인증 관련 시큐리티 추가

--- src/main/java/com/example/sns/config/SecurityConfig.java
+++ src/main/java/com/example/sns/config/SecurityConfig.java
@@ -1,18 +1,34 @@
 package com.example.sns.config;
 
+import com.example.sns.controller.RsData;
+import com.example.sns.exception.ErrorCode;
+import com.example.sns.exception.ErrorResult;
 import com.example.sns.jwt.JwtTokenFilter;
 import com.example.sns.jwt.JwtTokenUtils;
 import com.example.sns.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.example.sns.exception.ErrorCode.INVALID_PERMISSION;
 
 @Configuration
 @EnableWebSecurity
@@ -34,11 +50,38 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.GET).permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/users/join", "/api/v1/users/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/v1/users/**/role/change").hasRole("ADMIN")
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(new JwtTokenFilter(userService, secretKey, jwtTokenUtils), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                // 인증 실패 시 INVALID_PERMISSION 에러 발생
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        makeErrorResponse(response, INVALID_PERMISSION);
+                    }
+                })
+                // 인가 실패 시 INVALID_PERMISSION 에러 발생
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+                        makeErrorResponse(response, INVALID_PERMISSION);
+                    }
+                })
+                .and()
                 .build();
     }
+
+    // Security Filter Chain에서 발생하는 Exception은 ExceptionManager 까지 가지 않기 때문에 여기서 직접 처리
+    public void makeErrorResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(response.getWriter(), RsData.error(new ErrorResult(errorCode, "사용자가 권한이 없습니다.")));
+    }
+
 }
