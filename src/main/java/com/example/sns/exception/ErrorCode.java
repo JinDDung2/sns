da4cf5cf8950f0c8be69a34071dfe688355766fe
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-01-09 09:37:51 +0900

    [Feat] ErrorCode 접근 권한, 알람 존재 에러 추가

--- src/main/java/com/example/sns/exception/ErrorCode.java
+++ src/main/java/com/example/sns/exception/ErrorCode.java
@@ -12,12 +12,15 @@ public enum ErrorCode {
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"UserName을 찾을 수 없습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 잘못되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    ROLE_FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러입니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글이 없습니다."),
     DUPLICATED_LIKE(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다."),
-    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 한 적이 없습니다.");
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 한 적이 없습니다."),
+    ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "알람을 한 적이 없습니다.");
+
 
     private HttpStatus httpStatus;
     private String message;
